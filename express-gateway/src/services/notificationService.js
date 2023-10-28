09b94fcece10aff0681a979f2bdfd3ddbd23d015
Frederick <frederickankamah988@gmail.com>
2023-10-28 09:30:23 +0000

    initial commit

---
+++ express-gateway/src/services/notificationService.js
@@ -0,0 +1,10 @@
+
+const sendEmailNotification = (emailDetails) => {
+    // logic to send email notification
+};
+
+const sendSMSNotification = (smsDetails) => {
+    // logic to send SMS notification
+};
+
+module.exports = { sendEmailNotification, sendSMSNotification };
---
+++ express-gateway/src/services/paymentService.js
@@ -0,0 +1,10 @@
+
+const initiatePayment = (paymentDetails) => {
+    // logic to initiate a payment transaction
+};
+
+const validatePayment = (paymentId) => {
+    // logic to validate payment status
+};
+
+module.exports = { initiatePayment, validatePayment };
